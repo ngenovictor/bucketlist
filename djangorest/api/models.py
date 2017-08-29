1250d7bab3ce0920316033ffa424a82aaaffb798
Victor Ngeno <ngenovictor321@gmail.com>
2017-08-29 10:10:49 +0300

    create Model BucketList and its Tests

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class BucketList(models.Model):
+    """
+    Defines the specs for out bucketlist model
+    """
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """
+        returns human readable format of the model
+        """
+        return self.name
--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,4 +1,22 @@
 from django.test import TestCase
+from .models import BucketList
 
-# Create your tests here.
 
+class ModelTestCase(TestCase):
+    """
+    This is the test suite for the BucketList Model Class
+    """
+    def setUp(self):
+        """
+        define test client and other variables
+        """
+        self.bucketlist_name = "Write world class code"
+        self.bucketlist = BucketList(name = self.bucketlist_name)
+    def test_model_can_create_a_bucket_list(self):
+        """
+        Test that the bucketlist model can create a bucketlist
+        """
+        old_count = BucketList.objects.count()
+        self.bucketlist.save()
+        new_count = BucketList.objects.count()
+        self.assertNotEqual(old_count,new_count, "New count and Old count should not be equal")
